e883280a0edffdfd65c359d0e8577980287b6a20
rikki_iki <rikki.bochow@gmail.com>
2016-12-07 15:25:21 +1100

    Fix responsiveness

--- kss-assets/offscreen.js
+++ kss-assets/offscreen.js
@@ -1,13 +1,13 @@
 (function () {
 
   // Define variables.
-  var $toggleLink = $('.kss-offscreen__toggle');
-  var $offscreen = $('.kss-offscreen');
+  var $toggleLink   = $('.kss-offscreen__toggle');
+  var $offscreen    = $('.kss-offscreen');
+  var desktopWidth  = 769;
 
-  // Open the menu on page load.
-  var toggleOpen = true;
-  $offscreen.addClass('is-moved');
-  $toggleLink.addClass('is-active').attr('aria-expanded', 'true');
+  // Toggle open by default.
+  // Classes set in index.hbs
+  var toggleOpen    = true;
 
   // Do the toggle.
   var toggleMenu = function () {
@@ -22,14 +22,22 @@
     toggleOpen = !toggleOpen;
   };
 
+  // Open the menu when clicking the toggle link.
   $toggleLink.click(function () {
     toggleMenu();
     return false;
   });
 
+  // Close the menu on page load if desktop width.
+  if (window.innerWidth <= desktopWidth) {
+    toggleOpen = true;
+    toggleMenu();
+  }
+
   // Close the menu on on the templates section.
   var sectionTitle = $offscreen.find('h1.kss-title').text();
-  if (sectionTitle.indexOf('Templates') >= 0) {
+  if (sectionTitle.indexOf('Templates') >= 0 && window.innerWidth >= desktopWidth) {
+    toggleOpen = true;
     toggleMenu();
   }
 
